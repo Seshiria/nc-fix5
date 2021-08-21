5a52532e17354313c8adf750ea459da7307edbeb
Seshiria <57834968+Seshiria@users.noreply.github.com>
2021-08-21 17:35:01 +0800

    提交脚本和说明文件

---
+++ main.sh
@@ -0,0 +1,62 @@
+echo "修复nextcloud更新卡在步骤5的小工具"
+echo "使用本工具前请先手动更新一次nextcloud"
+echo "指定路径请直接在脚本后跟路径位置"
+WPATH=${WPATH:-$(pwd)}
+case $(find ${WPATH} -type f -name .step | wc -l) in
+0)
+    echo "找不到对应的更新文件！"
+    echo "当前操作的目录为：${WPATH}"
+    exit 1
+    ;;
+1)
+    ;;
+*)
+    echo "找到多于一个的更新目录，请清空更新缓存"
+    exit 1
+    ;;
+esac
+STEOFILE=$(find ${WPATH} -type f -name .step)
+if grep \"step\"\:5 ${STEOFILE} > /dev/null ; then
+    if grep \"state\":\"start\" ${STEOFILE} > /dev/null ; then
+        echo "step文件检查通过"
+    else
+        echo "当前nextcloud更新并没有开始运行或者已经运行完毕。下面为调试信息"
+        cat ${STEOFILE}
+        dirname ${STEOFILE}
+        exit 1
+    fi
+else
+    echo "当前nextcloud更新并不是在第五阶段，无法修复"
+    cat ${STEOFILE}
+    dirname ${STEOFILE}
+fi
+UPATH=$(dirname ${STEOFILE})
+UFILE=${UPATH}/downloads/*.zip
+case `ls ${UFILE}|wc -l` in
+1)
+    LOACL_SHA=`sha512sum ${UFILE}|awk -F "\ " '{print $1}'`
+    _NAME=`basename ${UFILE}`
+    ONLINME_SHA=`wget -q -O- https://download.nextcloud.com/server/releases/${_NAME}.sha512|awk -F "\ " '{print $1}'`
+    if [ "$ONLINME_SHA" = "$LOACL_SHA" ]; then
+        echo "哈希检查通过，正在写入信息"
+        echo -e "{\"state\":\"end\",\"step\":5}" > ${STEOFILE}
+        echo "写入完成，请重新执行nextcloud的更新操作，现在nextcloud更新会从第五步继续"
+        exit 0
+    else
+        echo "哈希对比出错，错误的更新文件"
+        echo "待更新的文件： ${_NAME}"
+        echo "本地sha512： ${LOACL_SHA}"
+        echo "远端sha512： ${ONLINME_SHA}"
+        echo "需要手动下载对应的更新文件到指定位置"
+        echo "可以尝试以下操作：------------------"
+        echo "rm ${UPATH}/downloads/*.zip"
+        echo "wget -P ${UPATH}/downloads/ https://download.nextcloud.com/server/releases/${_NAME}"
+    fi
+;;
+*)
+    echo "下载目录错误,不存在更新文件或存在多个更新文件。"
+    ls ${UFILE}
+esac
+
+
+
---
+++ readme.md
@@ -0,0 +1,59 @@
+# nextcloud update step 5 
+
+这个工具修复nextcloud的更新在第五步骤卡住的问题。
+
+如果你使用nextcloud的web出现下面这个问题
+
+````
+Verifying integrity
+Parsing response failed.
+Show detailed response
+````
+
+![webstop](\img\webstop.png)
+
+或者命令行工具卡在这个位置
+
+````
+[ ] Verify integrity ...PHP Fatal error:  Allowed memory size of 134217728 bytes exhausted (tried to allocate 166462960 bytes) in phar:///webapps/www/updater/updater.phar/lib/Updater.php on line 637
+````
+
+
+
+![shellstop](\img\shellstop.png)
+
+就可以使用这个工具进行修复。
+
+
+
+## 使用方法
+
+脚本依赖：wget，sha512sum等。在docker的alpine容器上测试通过。
+
+**注意：使用本工具前请先备份好自己的数据，并且需要在web或者命令行界面进行一次更新操作**
+
+先下载本项目的main.sh
+
+然后在命令行运行
+
+````shell
+#如果nextcloud的data目录在当前目录的子目录，就直接运行即可
+sh main.sh
+#如果要指定目录请使用
+#export WPATH=包含nextcoud data的目录
+#比如
+export WPATH=/
+sh main.sh
+````
+
+等命令行执行完毕无报错后，可以**重新**运行nextcloud的更新程序，会从第五步继续开始
+
+![webfix](\img\webfix.png)
+
+![webfix](\img\shelfix.png)
+
+## 问题原因和修复原理
+
+大概是nextcloud官方利用纯php实现来进行文件验证，占用大量内存导致php出错。
+
+这个工具用shell代替验证文件过程和把正确的状态写入对应文件。
